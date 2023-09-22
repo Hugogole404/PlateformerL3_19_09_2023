f12c815d08b5b98c773e26b2313e61d07045c9a2
P3D\hugo.lestriez <hugo.lestriez@lacatholille.fr>
2023-09-22 11:54:13 +0200

    Double saut en preparation mais marche pa encorze

--- PlateformerL3/Assets/Scripts/PlayerController.cs
+++ PlateformerL3/Assets/Scripts/PlayerController.cs
@@ -24,31 +24,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _timeMinBetweenJump;
     [SerializeField] float _jumpForce;
     [SerializeField] float _velocityFallMin;
-    [SerializeField] [Tooltip("Gravity when the player goes up and press jump")] private float _gravityUpJump;
+    [SerializeField][Tooltip("Gravity when the player goes up and press jump")] private float _gravityUpJump;
     [SerializeField] float _jumpInputTimer = 0.1f;
     [SerializeField] Collider2D[] _collidersGround;
     [SerializeField] float _timerNoJump;
-    [SerializeField] [Tooltip("Gravity otherwise")] private float _gravity;
+    [SerializeField][Tooltip("Gravity otherwise")] private float _gravity;
     [SerializeField] float _timeSinceJumpPressed;
     [SerializeField] float _timeSinceGrounded;
     [SerializeField] float _coyoteTime;
     [SerializeField] PhysicsMaterial2D _physicsFriction;
     [SerializeField] PhysicsMaterial2D _physicsNoFriction;
-    [SerializeField] int JumpTank;
+    [SerializeField] int _jumpTankMax;
+    [SerializeField] int _currentJumpTank;
 
     [Header("Slope")]
-    //[SerializeField] Collider2D[] _collider;
     [SerializeField] Collider2D _collider;
-    private RaycastHit2D[] _hitResults;
+    private RaycastHit2D[] _hitResults = new RaycastHit2D[2];
     private float _slopeDetectOffset;
     [SerializeField] private bool _isOnSlope;
 
-    [Header("Corner")]
-    [SerializeField] float[] direction;
-    [SerializeField] private BoxCollider2D _offsetCollisionBox;
-    [SerializeField] private BoxCollider2D _offsetToReplace;
-    [SerializeField] private BoxCollider2D offsetToReplace;
-    [SerializeField] private Vector2 _collisionBox;
+    //[Header("Corner")]
+    //[SerializeField] float[] direction;
+    //[SerializeField] private BoxCollider2D _offsetCollisionBox;
+    //[SerializeField] private BoxCollider2D _offsetToReplace;
+    //[SerializeField] private BoxCollider2D offsetToReplace;
+    //[SerializeField] private Vector2 _collisionBox;
 
     void HandleInputs()
     {
@@ -58,6 +58,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             _timeSinceJumpPressed = 0;
+            _currentJumpTank--;
         }
     }
     void HandleMovements()
@@ -75,11 +76,15 @@ public class PlayerController : MonoBehaviour
         Vector2 pointGround = (transform.position + Vector3.up * _groundOffset);
         bool currentGrounded =
             Physics2D.OverlapCircleNonAlloc(pointGround, _groundRadius, _collidersGround, _GroundLayer) > 0;
-        if(currentGrounded == false && _isGrounded)
+        if (currentGrounded == false && _isGrounded)
         {
             _timeSinceGrounded = 0;
         }
         _isGrounded = currentGrounded;
+        if (_isGrounded)
+        {
+            _currentJumpTank = _jumpTankMax;
+        }
     }
 
 
@@ -94,50 +99,47 @@ public class PlayerController : MonoBehaviour
 
     void HandleJump()
     {
-        _timerNoJump -= Time.deltaTime;
-        if (_isGrounded == false)
-        {
-            if (_rb.velocity.y <= 0 || _isOnSlope)
+            _timerNoJump -= Time.deltaTime;
+            if (_isGrounded == false)
             {
-                _rb.gravityScale = _gravity;
+                if (_rb.velocity.y <= 0 || _isOnSlope)
+                {
+                    _rb.gravityScale = _gravity;
+                }
+                else
+                {
+                    _rb.gravityScale = _inputJump ? _gravityUpJump : _gravity;
+                }
             }
             else
             {
-                _rb.gravityScale = _inputJump ? _gravityUpJump : _gravity;
+                _rb.gravityScale = _gravity;
             }
-        }
-        else
-        {
-            _rb.gravityScale = _gravity;
-        }
-        if (_inputJump && _rb.velocity.y <= 0 && (_isGrounded || _timeSinceGrounded < _coyoteTime) && _timerNoJump <= 0 &&
-            _timeSinceJumpPressed < _jumpInputTimer)
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
-            _timerNoJump = _timeMinBetweenJump;
-        }
+            if ((_inputJump && _rb.velocity.y <= 0 && (_isGrounded || _timeSinceGrounded < _coyoteTime) && _timerNoJump <= 0 &&
+                _timeSinceJumpPressed < _jumpInputTimer) && _currentJumpTank > 0 )
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+                _timerNoJump = _timeMinBetweenJump;
+            }
 
 
-        // limiter vit chute : 
-        if (_rb.velocity.y < _velocityFallMin)
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, _velocityFallMin);
-        }
-        _timeSinceJumpPressed += Time.deltaTime;
+            // limiter vit chute : 
+            if (_rb.velocity.y < _velocityFallMin)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _velocityFallMin);
+            }
+            _timeSinceJumpPressed += Time.deltaTime;
     }
 
     void HandleSlope()
     {
         Vector3 origin = transform.position + Vector3.up * _groundOffset;
-        bool slopeRight =
-            Physics2D.RaycastNonAlloc(origin, Vector2.right, _hitResults, _slopeDetectOffset, _GroundLayer) > 0;
-        bool slopeLeft =
-            Physics2D.RaycastNonAlloc(origin, -Vector2.right, _hitResults, _slopeDetectOffset, _GroundLayer) > 0;
+        bool slopeRight = Physics2D.RaycastNonAlloc(origin, Vector2.right, _hitResults, _slopeDetectOffset, _GroundLayer) > 0;
+        bool slopeLeft = Physics2D.RaycastNonAlloc(origin, -Vector2.right, _hitResults, _slopeDetectOffset, _GroundLayer) > 0;
         _isOnSlope = (slopeRight || slopeLeft) && (slopeRight == false || slopeLeft == false);
         if (Mathf.Abs(_inputs.x) < 00.1f && (slopeLeft || slopeRight))
         {
-           _collider.sharedMaterial = _physicsFriction;
-            //_collider.sharedMaterial = _physicsFriction;
+            _collider.sharedMaterial = _physicsFriction;
         }
         else
         {
@@ -185,4 +187,4 @@ public class PlayerController : MonoBehaviour
         HandleGrounded();
         HandleSlope();
     }
-}                    
+}
