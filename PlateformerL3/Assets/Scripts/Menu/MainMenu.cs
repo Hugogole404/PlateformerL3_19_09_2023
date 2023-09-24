357bcf6a0bfe2223113b8ff376ac54cedd5bb062
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-09-24 23:09:17 +0200

    JE SUIS UN SAUCISSSSSOOOOOONNNNNNNNN !!!!!

--- PlateformerL3/Assets/Scripts/Menu/MainMenu.cs
+++ PlateformerL3/Assets/Scripts/Menu/MainMenu.cs
@@ -2,9 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    [SerializeField] Button _button;
+    [SerializeField] TMPro.TextMeshProUGUI Text;
+    [SerializeField] Image FadeImage;
+    [SerializeField] Color ColorInitial;
+    [SerializeField] Color ColorSelected;
+
+    public void LoadSceneGame()
+    {
+
+    }
+    public void LoadSceneOption()
+    {
+
+    }
+    public void LoadSceneCredits()
+    {
+
+    }
+    public void LoadSceneMenu()
+    {
+
+    }
+    public void QuitGame()
+    {
+
+    }
+
+    private void FadeTransition()
+    {
+
+    }
 }
